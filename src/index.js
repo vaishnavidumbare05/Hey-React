cd9a9e153a5672fdfd7c6706e9fde9386b184e97
vaishnavidumbare05 <vaishnavidumbare2004@gmail.com>
2024-08-07 21:57:34 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import {createRoot} from "react-dom/client";
+
+// const root=createRoot(document.getElementById("root"));
+
+//we also write as
+import ReactDOM from "react-dom/client";
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<h1>My First React APP🤩</h1>)
